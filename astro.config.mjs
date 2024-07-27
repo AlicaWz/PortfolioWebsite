699c3a5a14b91acfb040535ea8a3a485eda99595
Alica Wziontek <alica.wziontek@2282-012024.fritz.box>
2024-07-27 19:49:07 +0200

    feat(C4-Number): add numbers module

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,9 @@ export default defineConfig({
       enableFallbackComponent: true,
       components: {
         p1Generic: 'templates/P1-Generic',
-        c1Stage: 'components/modules/content/C1-Stage'
+        c1Stage: 'components/modules/content/C1-Stage',
+        c3Image: 'components/modules/content/C3-Image',
+        c4Numbers: 'components/modules/content/C4-Numbers',
       },
       apiOptions: {
         region: 'eu'
