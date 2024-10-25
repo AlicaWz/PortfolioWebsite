33ca2b28fe8efff5896c50bba133a3fb4d91d2ed
Alica Wziontek <Alica.Wziontek@db-n.com>
2024-10-25 15:28:10 +0200

    feat(chore) update structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,12 +21,12 @@ export default defineConfig({
       components: {
         p1Generic: 'templates/P1-Generic',
         p3AboutMe: 'templates/P3-AboutMe',
-        c1Stage: 'components/modules/content/C1-Stage',
-        c2Text: 'components/modules/content/C2-Text', 
-        c21Text: 'components/modules/content/C2.1-Text',
-        c3Image: 'components/modules/content/C3-Image',
-        c4Numbers: 'components/modules/content/C4-Numbers',
-        c5Timeline: 'components/modules/content/C5-Timeline',
+        c1Stage: 'components/modules/content/C1-Stage/index',
+        c2Text: 'components/modules/content/C2-Text/index', 
+        c21Text: 'components/modules/content/C2.1-Text/index',
+        c3Image: 'components/modules/content/C3-Image/index',
+        c4Numbers: 'components/modules/content/C4-Numbers/index',
+        c5Timeline: 'components/modules/content/C5-Timeline/index',
         t2Overview: 'components/modules/teaser/T2-Overview'
       },
       apiOptions: {
